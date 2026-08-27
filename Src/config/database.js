080a50e52610d74cc0feb9b515160e9fc452006f
Krishna77 <krishnkantchaurasia8889@gmail.com>
2026-08-28 00:59:18 +0530

    handled environment variables

--- Src/config/database.js
+++ Src/config/database.js
@@ -5,12 +5,12 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://krishnkantchaurasia8889_db_user:OdNQMF6KkmBCg5aR@project01database.nc33jdl.mongodb.net/devTinder",
-  );
+    await mongoose.connect(
+        process.env.MONGODB_URI,
+    );
 };
 
-module.exports = {connectDB}
+module.exports = { connectDB }
 
 // connectDB().then(() =>{
 //     console.log("db connection is succesful")
