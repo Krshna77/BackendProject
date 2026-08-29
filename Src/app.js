66805ea2e0e450a9ebe95ccda23cbd2bf8e927ac
Krishna77 <krishnkantchaurasia8889@gmail.com>
2026-08-29 20:48:56 +0530

    get and post api for getting the user and saving the user in DB

--- Src/app.js
+++ Src/app.js
@@ -6,24 +6,48 @@ const { connectDB } = require('./config/database')
 const User = require('./models/user')
 
 
+app.use(express.json())
 
 app.post('/signUp', async (req, res) => {
-    const user = new User({
-        firstName: "krishna",
-        lastName: "chaurasia",
-        email: "krishna@gmail.com",
-        age: 19,
-        gender: "male"
-    })
+    console.log(req.body)
+    const user = new User(req.body)
 
     try {
         await user.save();
         res.send("user added successfully")
-    }catch(err){
+    } catch (err) {
         res.status(400).send("error saving user" + err)
     }
 
-    
+
+})
+
+app.get('/getUser', async (req, res) => {
+    const userEmail = req.body.email;
+
+    try {
+        const user = await User.find({ email: userEmail });
+        if (user.length === 0) {
+            res.status(400).send("user not found")
+        } else {
+            res.send(user);
+        }
+    } catch(err) {
+        res.status(400).send("error saving user" + err)
+
+    }
+
+})
+
+app.get('/feed', async (req, res) => {
+
+
+    try {
+        const user = await user.find({});
+        res.send(user);
+    }catch(err){
+        res.status(400).send("error saving user" + err)
+    }
 })
 
 
